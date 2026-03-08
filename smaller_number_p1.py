628be9dc31ffb1db3866a6c325cfcb73cd595725
ps-yanga <yangaprincess03@gmail.com>
2026-03-09 01:46:45 +0800

    smaller number

---
+++ smaller_number_p1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+if num1 < num2:
+    print("THE SMALLER NUMBER IS", num1)
+else:
+    print("THE SMALLER NUMBER IS", num2)
