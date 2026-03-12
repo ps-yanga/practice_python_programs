1754f43d564836764080c3271f5fce639a5e1cb7
ps-yanga <yangaprincess03@gmail.com>
2026-03-13 00:49:03 +0800

    batch 1

---
+++ one_batch/bigger_number_b1_p1.py
@@ -0,0 +1,8 @@
+# number 1
+num1 = int(input("Enter  first number: "))
+# number 2
+num2 = int(input("Enter  second number: "))
+if num1 > num2:
+    print("the bigger number is", num1)
+else:
+    print("the bigger number is", num2)
