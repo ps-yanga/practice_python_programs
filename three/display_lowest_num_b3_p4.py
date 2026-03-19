470913e9519330baced98fc7172cda6937329558
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 00:23:38 +0800

    lowest

--- three/display_lowest_num_b3_p4.py
+++ three/display_lowest_num_b3_p4.py
@@ -1,1 +1,8 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+numbers = []
+while True:
+    inp = input("Enter number: ")
+    if not inp.isdigit():
+        break
+    numbers.append(int(inp))
+print(min(numbers) if numbers else "No numbers")
