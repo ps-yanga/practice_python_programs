031f679ec0ffb6e96321e5612ec74431d1f6448f
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 00:33:53 +0800

    sort

--- three/using_sort_b3_p5.py
+++ three/using_sort_b3_p5.py
@@ -1,1 +1,9 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+numbers = []
+while True:
+    inp = input("Enter number: ")
+    if not inp.isdigit():
+        break
+    numbers.append(int(inp))
+numbers.sort()
+print(*numbers, end=", ")
