befd74da6ef5af05b9f270f1274c64af41fd0edc
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 02:00:15 +0800

    h to l

--- four/highest_to_lowest_b4_p4.py
+++ four/highest_to_lowest_b4_p4.py
@@ -1,1 +1,9 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+numbers = []
+while True:
+    inp = input("Enter number: ")
+    if not inp.isdigit():
+        break
+    numbers.append(int(inp))
+numbers.sort(reverse=True)
+print(*numbers)
