ddb400c4d8d20469ffedc10f0077e5f1fb8d0614
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 01:36:46 +0800

    most

--- four/most_number_dups_b4_p2.py
+++ four/most_number_dups_b4_p2.py
@@ -1,1 +1,10 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+from collections import Counter
+numbers = []
+while True:
+    inp = input("Enter number: ")
+    if not inp.isdigit():
+        break
+    numbers.append(int(inp))
+most_common = Counter(numbers).most_common(1)[0][0]
+print("most duplicate:", most_common)
