fdbc83713f9151993346255ed2e20a8d5964899b
ps-yanga <yangaprincess03@gmail.com>
2026-03-20 16:00:44 +0800

    initial

---
+++ five/no_space_in_front_b5_p1.py
@@ -0,0 +1,4 @@
+# Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+# Example:
+# Input:         Juan Dela Cruz
+# Output: Juan Dela Cruz
